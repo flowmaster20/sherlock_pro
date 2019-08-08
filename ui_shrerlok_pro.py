f21dab4d6801951a3a351c928b4206a68ca48aa3
Piotr <draboolpl@gmail.com>
2019-08-08 15:10:43 +0200

    ui takie pierwze cza jesze wymyślić jak się robi tabele ale zrobię to wieczorkiem

---
+++ ui_shrerlok_pro.py
@@ -0,0 +1,19 @@
+import json as j
+
+print("helloworld!")
+
+# arkusz = open("arkusz.json")
+# print(arkusz)
+
+
+name = input("paste name:")
+surname = input("paste surname:")
+l_number = input("paste any known lucky numbers:")
+nickname = input("paste any known nicknames:")
+print("date format must be days.monts.year like: 24.04.1998")
+birthday_date = input("paste birthday date:")
+
+print("date format must be days.monts.year like: 24.04.1998")
+pet_name = input("paste any known pet names:")
+known_username = input("paste any known usernames, that were used on socjal media before:")
+print(name + " " + surname + " " + l_number + " " + nickname + " " + birthday_date + " " + pet_name + " " + known_username + " ")
