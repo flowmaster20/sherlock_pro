771cd582062a40fcebd1dfe12faddb5f3ea075bc
Piotr <draboolpl@gmail.com>
2019-08-08 21:17:37 +0200

    ui takie pierwze cza jesze wymyślić jak się robi tabele

--- ui_shrerlok_pro.py
+++ ui_shrerlok_pro.py
@@ -12,8 +12,6 @@ l_number = input("paste any known lucky numbers:")
 nickname = input("paste any known nicknames:")
 print("date format must be days.monts.year like: 24.04.1998")
 birthday_date = input("paste birthday date:")
-
-print("date format must be days.monts.year like: 24.04.1998")
 pet_name = input("paste any known pet names:")
 known_username = input("paste any known usernames, that were used on socjal media before:")
 print(name + " " + surname + " " + l_number + " " + nickname + " " + birthday_date + " " + pet_name + " " + known_username + " ")
