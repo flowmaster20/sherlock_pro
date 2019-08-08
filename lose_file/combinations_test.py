c3499c1b4eef31edfc8c97f1b9a970f0aac3f45e
flowmaster20 <mkojro20@gmail.com>
2019-08-08 23:49:37 +0200

    proby walki z tworzeniem wszytskich mozliwych nazw

---
+++ lose_file/combinations_test.py
@@ -0,0 +1,13 @@
+tab = []
+for i in range(5**5):
+    a = str(int(i/1)%5)
+    b = str(int(i/2)%5)
+    c = str(int(i/3)%5)
+    d = str(int(i/4)%5)
+    e = str(int(i/5)%5)
+
+    data = a+b+c+d+e
+    print(data)
+    tab.append(data)
+tab = set(tab)
+print (5**5,len(tab),len(tab)/5**5)
