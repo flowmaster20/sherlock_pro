20c08cdc3aab5bad74c751a39d761b9559b9f806
flowmaster20 <mkojro20@gmail.com>
2019-08-09 01:37:15 +0200

    little shuffle in names an files

--- functions.py
+++ functions.py
@@ -3,6 +3,26 @@ def create_mutations(word):
     for i in range(len(word)):
         tab.append(word[:i+1]) #dla kazdej mozliwej dlugosci slowa tworze osobna zmienna i dodaje ja do tablicy ktora zwracam
     return tab
+
+def prepare(name, surname, l_number, nickname, birthday_date, pet_name, known_username):
+
+    #dodaje wszytskie elementy i tablice w jedna
+    all_possibilites = l_number + nickname + birthday_date + pet_name + known_username
+    all_possibilites.append(name)
+    all_possibilites.append(surname)
+
+
+    #dla kazdego slowa tworze jego mutacje tzn auto --> a ,au ,aut ,auto
+    everything = []
+    for word in all_possibilites:#idzie przez wszytskie slowa
+        everything = everything + create_mutations(word)#robi mutacje slowa word
+    everything = everything #+ standard_set#dodaje standard set w zamysle rzeczy takie jak kropki podkreslnki itd ktore beda takie same zawsze
+    everything = set(everything)#usuwa powtorzenia i sortuje
+    #print(everything)
+    return everything
+
+    #functions.create_all_combinations(everything)#podzielenie na pod tablice to multithreding
+
 """
 def create_all_combinations(tab,how_many):
     for i in range(how_many**len(tab)): #jakis pomysl na laczenie wszytskich wyrazow do siebie ale na razie jeszcze sam nie wiem jak to ma dzialac do konca
--- test.py
+++ test.py
@@ -10,10 +10,10 @@ class TestSherlockPro(unittest.TestCase):
         msg = "should be [\"d\",\"do\",\"dom\"]"
         self.assertEqual(create_mutations(input),ex_output,msg)
 
-    def test_run(self):
+    def test_prepare(self):
         ex_output = {'mkoj', 'mateusz', '3', '2000', 'mateu', 'mkojro', 'koj', '0', 'mara', 'mateus', 'mkojro2', 'kojro', '2', '1', 'matr', '16', 'k', '200', 'marato', 'kojr', 'mate', 'matri', 'mat', 'mkojro20', '18', '20', '30', 'mati', 'matrix', 'ko', 'mk', '06', 'mko', 'mkojr', 'm', 'ma', 'mar', 'marat', 'maraton'}
         msg = "wrong output from main function"
-        self.assertEqual(run("mateusz","kojro",["16","18"],["mati"],["30","06","2000"],["maraton"],["mkojro20","matrix"]),ex_output,msg)
+        self.assertEqual(prepare("mateusz","kojro",["16","18"],["mati"],["30","06","2000"],["maraton"],["mkojro20","matrix"]),ex_output,msg)
 
 
 
