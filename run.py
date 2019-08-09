8ab56292cf4e06cb9b49a74aa56793c5d45c7869
flowmaster20 <mkojro20@gmail.com>
2019-08-09 02:17:09 +0200

    repaired idiotical combinations cretions

--- run.py
+++ run.py
@@ -3,4 +3,8 @@ import functions
 
 if __name__ == '__main__':
     name, surname, l_number, nickname, birthday_date, pet_name, known_username = get_data()
-    everything = functions.prepare(name, surname, l_number, nickname, birthday_date, pet_name, known_username);
+    everything = functions.prepare(name, surname, l_number, nickname, birthday_date, pet_name, known_username)
+    all = functions.idioticly_create_combinations(list(everything))
+
+    for word in everything:
+        print(word,"\n")
