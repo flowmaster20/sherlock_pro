2a76b8c7b0e2366d3d36f8b8df26c95251d4703c
mateuszkojro <mateusz.kojro@unilodz.eu>
2019-11-09 22:46:24 +0100

    fixed bugs

--- looker/pararel.py
+++ looker/pararel.py
@@ -1,13 +1,13 @@
-#import sherlock
+import sherlock
 from sherlock import main
 from multiprocessing import Pool
 
 tab = ["abc","abcd","abcd","abcdd"]
-
+"""
 for usr in tab:
     main(usr)
-
 """
+
 with Pool(8) as p:
     print(p.map(sherlock.main,tab))
-"""
+
